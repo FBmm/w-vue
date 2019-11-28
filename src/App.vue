876153e12369c614ff8bc1bacabe0f7147a7b379
W9005349 <W9005349@adc.com>
2019-11-28 09:20:25 +0800

    app.vue文件优化

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <!-- <router-link to="/login">login</router-link> -->
-    <!-- <router-link to="/home">home</router-link> -->
     <router-view />
   </div>
 </template>
