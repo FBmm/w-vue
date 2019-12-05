446edf46e14387545989aff503176ff593d2410c
W9005349 <W9005349@adc.com>
2019-12-05 09:26:54 +0800

    【组件库demo】选择器插件

---
+++ src/views/demo/MultiSelect/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <OpMultiSelect :multiple="multiple" :group="group" :data="data" :showSelectedCount="true" :selected="['00','02','11']" @onClose="showData" ></OpMultiSelect>
+    </div>
+</template>
+<script>
+import OpMultiSelect from "@/@ang/MultiSelect"
+export default {
+    name: 'multiSelectDemo',
+    components: {OpMultiSelect},
+    data() {
+        const res = {
+            multiple: true,
+            group: true,
+            data: []
+        }
+
+        var labels = ['未归档','已归档'];
+        for(let i = 0; i < labels.length;i++) {
+            res.data.push({
+                label: labels[i],
+                options: []
+            });
+            for(let j = 0;j<10;j++) {
+                res.data[i].options.push({
+                    label: `${labels[i]}迭代${j}`,
+                    value: `${i}${j}`
+                })
+            }
+        }
+        return res;
+    },
+    methods: {
+        showData(val) {
+            console.log(val)
+        },
+    }
+}
+</script>
